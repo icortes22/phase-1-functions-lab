96836a72d45263d9493043115bcfdda8a472539e
Isamar <isamarcortes589@gmail.com>
2021-10-02 23:32:51 -0400

    commit tree 5336b79246ad5b3ec49337355b17e4dc7929bbf9

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(b){
+    if(b <= 42){
+        return 42 - b
+    }else if(b >= 42){
+        return b - 42
+    }
+}
+function distanceFromHqInFeet(f){
+  return distanceFromHqInBlocks(f) * 264
+}
+function distanceTravelledInFeet(start, end){
+  return Math.abs(start - end) * 264
+}
+function calculatesFarePrice(start, end){
+  let blocks = Math.abs(start - end)
+  let distance = blocks * 264
+  if (distance <= 400){
+    return 0
+  }else if(distance > 2500){
+    return "cannot travel that far"
+  }else if(400 < distance && distance < 2000){
+    let ride = 0.02 * (distance - 400)
+    return ride
+  }else if(2000 <= distance < 2500){
+    return 25
+  }
+}
